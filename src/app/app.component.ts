02d2039053b328a55fcbf1df55c2442819407e48
PatricioRubial <91898476+PatricioRubial@users.noreply.github.com>
2022-08-30 12:06:20 -0300

    Tour of Heroes-add navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-proyect';
+  title = 'Tour of Heroes';
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr. Nice' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+
+  { id: 21, name: 'ManRubber' },
+  { id: 22, name: 'Amanyd' },
+  { id: 23, name: 'Dr. IQ' },
+  { id: 24, name: 'Magma' },
+  { id: 25, name: 'Qi.DR' },
+  { id: 26, name: 'Amgam' },
+  { id: 27, name: 'Odanrot' },
+  { id: 28, name: 'Atengam' },
+  { id: 29, name: 'Otsabmob' },
+  { id: 30, name: 'Ecin.Rd' }
+];
